a40c180285154e8689a764684848d0a8d3dabb0f
Unique Orduna <uniqueorduna@uniques-air.attlocal.net>
2022-05-06 21:44:45 -0700

    structured-english

---
+++ test_2.py
@@ -0,0 +1,14 @@
+"""
+input:class grades from file final.txt
+iniate a iterator and accumulator and set it to zero
+loop through the list of grades 
+add all the grades
+once the loop ends, there are no more scores to add
+divide the accumulator by the counter 
+diplay the class grade average
+initiate accumulator to assess how many grades are above average
+output: display percentage of grades above average
+
+
+
+"""
